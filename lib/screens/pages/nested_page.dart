17db66121300481b9eb5d3e6484b1725e4220463
Anik Mosfeq <mosfeqanik01@gmail.com>
2022-01-23 14:46:38 +0600

    list view +silverslist +silver appbar + silver Grid added

---
+++ lib/screens/pages/nested_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:layouts_practice_flutter/screens/pages/main_app_bar.dart';
+
+import '../layout_type.dart';
+
+
+class NestedPage extends StatelessWidget implements HasLayoutGroup {
+  NestedPage({Key key, this.layoutGroup, this.onLayoutToggle})
+      : super(key: key);
+  final LayoutGroup layoutGroup;
+  final VoidCallback onLayoutToggle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MainAppBar(
+        layoutGroup: layoutGroup,
+        layoutType: LayoutType.nested,
+        onLayoutToggle: onLayoutToggle,
+      ),
+      body: Container(
+        child: _buildContent(),
+      ),
+    );
+  }
+
+  Widget _buildContent() {
+    return ListView.builder(
+        itemCount: 16,
+        itemBuilder: (BuildContext content, int index) {
+          return _buildHorizontalList(parentIndex: index);
+        });
+  }
+
+  Widget _buildHorizontalList({int parentIndex}) {
+    var colors = [
+      Colors.green,
+      Colors.blue,
+      Colors.indigo,
+      Colors.red,
+      Colors.orange
+    ];
+    double height = 136.0;
+    return SizedBox(
+      height: height,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 20,
+          itemBuilder: (BuildContext content, int index) {
+            return _buildItem(
+              index: index + 1,
+              color: colors[(parentIndex + index) % colors.length],
+              parentSize: height,
+            );
+          }),
+    );
+  }
+
+  Widget _buildItem({int index, Color color, double parentSize}) {
+    double edgeSize = 8.0;
+    double itemSize = parentSize - edgeSize * 2.0;
+    return Container(
+      padding: EdgeInsets.all(edgeSize),
+      child: SizedBox(
+        width: itemSize,
+        height: itemSize,
+        child: Container(
+          alignment: AlignmentDirectional.center,
+          color: color,
+          child: Text(
+            '$index',
+            style: TextStyle(fontSize: 72.0, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
